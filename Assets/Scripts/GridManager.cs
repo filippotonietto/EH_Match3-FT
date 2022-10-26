d89e154d2280ffcfd9678ea5884a957aad7f853b
Filippo Tonietto <filippo.tonietto@gmail.com>
2022-10-26 09:11:55 +0200

    Customizable Grid with Camera adjustments.

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -9,6 +9,8 @@ public class GridManager : MonoBehaviour
     public GameObject cellObject;
     public float scaleMultiplier;
 
+    public List<GameObject> cells = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,20 +25,29 @@ public class GridManager : MonoBehaviour
 
     public void GenerateGrid()
     {
-        AdjustCamera();
-
         for(int i = 0; i < gridHeight; i++)
         {
             for(int j = 0; j < gridWidth; j++)
             {
                 GameObject newCell = Instantiate(cellObject, GameObject.Find("Cells").transform);
                 newCell.transform.localPosition = new Vector3(i * scaleMultiplier, j * -scaleMultiplier, 0);
+                cells.Add(newCell);
             }
         }
+
+        AdjustCamera();
     }
 
     public void AdjustCamera()
     {
+        Vector3 firstCellPos = cells[0].transform.position;
+        int n = cells.Count;
+        Vector3 lastCellPos = cells[n - 1].transform.position;
+        float zoom = Mathf.Max(gridWidth, gridHeight);
+
+        Vector3 midPoint = new Vector3((firstCellPos.x + (lastCellPos.x - firstCellPos.x) / 2), (firstCellPos.y + (lastCellPos.y - firstCellPos.y) / 2), -1f);
+        Camera.main.transform.position = midPoint;
+        Camera.main.orthographicSize = zoom;
     }
 
     public void PlaceCell()
